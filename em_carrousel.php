66ef9f0b5c5f1677adcecc6188936c6493cf3cda
Rosember10 <rosember.01@outlook.com>
2022-10-13 11:19:43 -0400

    c9-ajout du carrousel

--- em_carrousel.php
+++ em_carrousel.php
@@ -10,7 +10,19 @@
  */
 
  function genere_boite(){
-    $contenu = "<div class='carrousel' >carrusel</div>";
+    $contenu = "
+    <style>
+    .carrousel {
+        border:2px solid #124DB0;
+        background-color:#124DB0;
+        color : white;
+        width : 200px;
+        height :200px;
+        text-align: center;
+        margin: auto;
+    } 
+    </style>
+    <div class='carrousel' >carrusel</div>";
     return $contenu;
  };
  add_shortcode('rl_carrousel','genere_boite'); // para que el plugin funcione, el cual esta en la funcion genere_boite
