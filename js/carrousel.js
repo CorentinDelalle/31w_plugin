80a62ab3bbfa97099df3c3d45737e7bd07d02745
CorentinDelalle <corentindelalle@gmail.com>
2022-10-18 16:52:51 -0400

    fonctions javascript

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,6 @@
 (function(){
     console.log("carrousel");
+    let index = 0;
     const emlBtnModal = document.querySelector('.btn_modal');
     const elmCarrousel = document.querySelector('.carrousel');
     const elmBtnModalFermer = document.querySelector('.btn_fermer')
@@ -10,14 +11,26 @@
 
     for (const elImg of elGalerie__img){
         ajouter_img_carrousel(elImg);
-        ajouter_radio_carrousel();
+        ajouter_radio_carrousel()
     }
+
 
     function ajouter_img_carrousel(elImg){
         let elCarrousel__figure__img = document.createElement('img');
         elCarrousel__figure__img.setAttribute('src',elImg.getAttribute('src'));
-        elCarrousel__figure__img.classList.add('elCarrousel__figure_img');
-        elCarrousel__figure__img.appendChild(elCarrousel__figure__img);
+        elCarrousel__figure__img.classList.add('carrousel__figure__img');
+        elCarrousel__figure.appendChild(elCarrousel__figure__img);
+    }
+
+    function ajouter_radio_carrousel(){
+        let elCarrousel__form__radio = document.createElement('input');
+        elCarrousel__form__radio.setAttribute('name','carrousel__form__radio');
+        elCarrousel__form__radio.setAttribute('class','carrousel__form__radio');
+        elCarrousel__form__radio.setAttribute('type','radio');
+        elCarrousel__form__radio.dataset.index = index;
+        index++;
+        elCarrousel__form.appendChild(elCarrousel__form__radio);
+
     }
 
     emlBtnModal.addEventListener('mousedown', function(){
